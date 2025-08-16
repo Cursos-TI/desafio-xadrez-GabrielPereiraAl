8993c83f49c7d04de009db29949116515c832ab3
Gabriel Pereira Alves <157809837+GabrielPereiraAl@users.noreply.github.com>
2025-08-16 13:25:24 +0000

    Desafio Aventureiro - Adicionei movimento do cavalo

--- xadrez.c
+++ xadrez.c
@@ -29,5 +29,18 @@ int main() {
         contRainha++;
     } while (contRainha < movimentoRainha);
 
+    //Movimento do Cavalo : for e while
+        
+            
+    int movimentoCavaloCima = 2;
+    printf("Movimento Cavalo:\n");
+        for (int movimentoCavaloLado = 0; movimentoCavaloLado < 1;movimentoCavaloLado++){
+            while (movimentoCavaloCima > 0){
+                printf("Cima,");
+                movimentoCavaloCima--;
+                }
+            printf("Direita\n");
+            }
+
     return 0;
 }

ba080de958b3ff6e156e585459af79f83a612488
Gabriel Pereira Alves <157809837+GabrielPereiraAl@users.noreply.github.com>
2025-08-18 17:18:50 +0000

    Desafio Mestre

--- xadrez.c
+++ xadrez.c
@@ -1,44 +1,70 @@
 #include <stdio.h>
 
 
+
+void movimentoTorre(int casas) {
+    if (casas <= 0) return;
+    printf("Direita\n");
+    movimentoTorre(casas - 1);
+}
+
+
+void movimentoBispo(int movimentoVertical, int movimentoHorizontal) {
+    for (int i = 0; i < movimentoVertical; i++) {
+        for (int j = 0; j < movimentoHorizontal; j++) {
+            printf("Cima Direita\n");
+        }
+    }
+}
+
+void movimentoRainha(int casas) {
+    if (casas <= 0) return;
+    printf("Esquerda\n");
+    movimentoRainha(casas - 1);
+}
+
+void movimentoCavalo(int maxCima, int maxDireita) {
+    for (int cima = 0; cima < maxCima; cima++) {
+        for (int direita = 0; direita < maxDireita; direita++) {
+            if (cima == 2 && direita == 1) {
+                printf("Cima\n");
+                printf("Cima\n");
+                printf("Direita\n");
+                break;
+            }
+            if (direita == 0) continue;
+        }
+    }
+    printf("\n");
+}
+
+
 int main() {
 
-    // Movimento da Torre - for
-    const int movimentoTorre = 5;
-    int contTorre;
+    //Varieaveis para Torre, Bisbo e Rainha
+    const int movimentoTorreCasas = 5;
+    const int movimentoBispoCasas = 5;
+    const int movimentoRainhaCasas = 8;
+
+    // Movimento da Torre
     printf("Movimento da Torre:\n");
-    for (contTorre = 0; contTorre < movimentoTorre; contTorre++) {
-        printf("Direita\n");
-    }
+    movimentoTorre(movimentoTorreCasas);
+    printf("\n");
 
-    // Movimento do Bispo : while
-    const int movimentoBispo = 5;
-    int contBispo = 0;
-    printf("\nMovimento do Bispo:\n");
-    while (contBispo < movimentoBispo) {
-        printf("Cima Direita\n");
-        contBispo++;
-    }
+    // Movimento do Bispo
+    printf("Movimento do Bispo:\n");
+    movimentoBispo(3, 3); 
+    printf("\n");
 
-    // Movimento da Rainha : do-while
-    const int movimentoRainha = 8;
-    int contRainha = 0;
-    printf("\nMovimento da Rainha:\n");
-    do {
-        printf("Esquerda\n");
-        contRainha++;
-    } while (contRainha < movimentoRainha);
+    // Movimento da Rainha
+    printf("Movimento da Rainha:\n");
+    movimentoRainha(movimentoRainhaCasas);
+    printf("\n");
 
-    //Movimento do Cavalo : for e while           
-    int movimentoCavaloCima = 2;
+    //Movimento do Cavalo          
+    printf("Movimento da Cavalo:\n");
     printf("Movimento Cavalo:\n");
-        for (int movimentoCavaloLado = 0; movimentoCavaloLado < 1;movimentoCavaloLado++){
-            while (movimentoCavaloCima > 0){
-                printf("Cima,");
-                movimentoCavaloCima--;
-                }
-            printf("Direita\n");
-            }
+    movimentoCavalo(3, 2);
 
     return 0;
 }
